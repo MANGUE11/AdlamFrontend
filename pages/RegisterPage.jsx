f222c04e444fae575ff81674a55d23d66d09fe5b
SyllaMangue13 <manguemasysylla13@gmail.com>
2025-11-02 13:24:51 +0100

    changement de endpoint dans register

--- pages/RegisterPage.jsx
+++ pages/RegisterPage.jsx
@@ -48,13 +48,16 @@ const RegisterPage = () => {
 
     try {
       // Simulation d'une requête API pour l'inscription
-      const response = await fetch('http://localhost:3000/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, password }),
-      })
+      const response = await fetch(
+        'https://adlambackend-production.up.railway.app/api/auth/register',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, email, password }),
+        }
+      )
 
       const data = await response.json()
 
